943dfcf2e466c3eb0c31056cbfc9682772fcc8b5
Aloysius Chng <2302857@sit.singaporetech.edu.sg>
2024-11-27 17:25:38 +0800

    Updated system
    Frontend and backend to give better UI/UX and fix overall functionality

--- app.py
+++ app.py
@@ -1,82 +1,80 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, jsonify
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.models import load_model
 import os
 import numpy as np
 import cv2
+import hashlib
+import PIL
+import io
 
 img_height=224
 img_width=224
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'uploads'  # Folder to save uploaded images
-os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+app = Flask(
+    __name__,
+    template_folder = 'templates',
+    static_folder = 'static'
+)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+os.makedirs(
+    app.config['UPLOAD_FOLDER'],
+    exist_ok = True
+)
 
-# Load your model
 model = load_model('best_val_acc_model.keras')
 
-def preprocess_image(image_path, target_size):
-    """Preprocess the image to match the model's expected input."""
-    img = load_img(image_path, target_size=target_size)  # Resize image
-    img_array = img_to_array(img) / 255.0  # Normalize to [0, 1]
-    img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
-    return img_array
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        # Handle the file upload
-        if 'file' not in request.files:
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            return redirect(request.url)
-        if file:
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-            file.save(file_path)  # Save the uploaded file
-            
-            # Preprocess and predict
-            processed_image = image_preprocessing(file_path)
-            predictions = model.predict(processed_image)
-            predicted_class_index = np.argmax(predictions, axis=1)[0]
-            result = map_class_to_age(predicted_class_index)
-
-            return f'Predicted class: {result}'
-
-    return '''
-    <!doctype html>
-    <title>Upload an Image</title>
-    <h1>Upload an Image for Prediction</h1>
-    <form method="post" enctype="multipart/form-data">
-        <input type="file" name="file">
-        <input type="submit" value="Upload">
-    </form>
-    '''
-
-# Image preprocessing function
 def image_preprocessing(img_path):
     '''
     Reads, resizes, and normalizes the image.
     '''
-    # Read the image
     img = cv2.imread(img_path)
     if img is None:
         raise ValueError(f"Image not found or invalid at path: {img_path}")
-
-    # Resize and normalize
     resized_img = cv2.resize(img, (img_height, img_width))
     normalized_img = resized_img / 255.0
+    img_array = img_to_array(normalized_img)
+    img_array = np.expand_dims(img_array, axis=0)
 
-    return normalized_img
-def map_class_to_age(class_index):
-    # Age ranges for the class indices
-    age_ranges = {
-        0: "0-24",
-        1: "25-49",
-        2: "50-74",
-        3: "75-99",
-        4: "100-124"
-    }
+    return img_array
 
-    return age_ranges.get(class_index, "Unknown")
+@app.route('/api/upload_file', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part', 'success': False}), 400
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'No selected file', 'success': False}), 400
+    if file is None:
+        return jsonify({'error': 'Invalid file', 'success': False}), 400
+    try:
+        file_image = PIL.Image.open( file )
+    except:
+        return jsonify({'error': 'Invalid image', 'success': False}), 400
+    file_image = file_image.convert('RGB').resize( (img_height, img_width) )
+    virtual_file = io.BytesIO()
+    file_image.save( virtual_file, format='PNG' )
+    
+    processed_file_hash = hashlib.sha256( virtual_file.getvalue() ).hexdigest()
+    processed_file_path = os.path.join( app.config['UPLOAD_FOLDER'], processed_file_hash + '.png' )
+    if not os.path.exists( processed_file_path ):
+        file_image.save( processed_file_path, format='PNG' )
+    return jsonify({'success': True, 'file_hash': processed_file_hash}), 200
+
+@app.route('/api/predict_age/<file_hash>', methods=['GET'])
+def get_file_prediction( file_hash : str ):
+    file_path = os.path.join( app.config['UPLOAD_FOLDER'], file_hash + '.png' )
+    if not os.path.exists( file_path ):
+        return jsonify({'error': 'File not found', 'success': False}), 404
+    
+    processed_image = image_preprocessing( file_path )
+    predictions = model.predict( processed_image )
+    predicted_class_index = np.argmax( predictions, axis = 1 )[0]
+    
+    return jsonify({ 'success': True, 'predicted_class': predicted_class_index.item() }), 200
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
---
+++ static/js/image_handler.js
@@ -0,0 +1,75 @@
+const age_ranges_index = {
+    0: "0-24",
+    1: "25-49",
+    2: "50-74",
+    3: "75-99",
+    4: "100-124"
+}
+
+document.addEventListener('DOMContentLoaded', async function() {
+    const loadingContainer = document.getElementById('loading-container');
+    const imageUploadButton = document.getElementById('image-upload-btn');
+    const predictButton = document.getElementById('predict-btn');
+    const imageUploadContainer = document.getElementById('image-upload-container');
+    const imagePreview = document.getElementById('image-preview');
+    const predictionResultsContainer = document.getElementById('prediction-results-container');
+    const predictedAgeLabel = document.getElementById('predicted-age-label');
+    const returnButton = document.getElementById('return-btn');
+
+    imageUploadButton.addEventListener('change', async function() {
+        if (imageUploadButton.files.length === 0) {
+            predictButton.disabled = true;
+            return;
+        }
+        predictButton.disabled = false;
+    });
+
+    predictButton.addEventListener('click', async function() {
+        if ( predictButton.disabled ) { return; }
+        loadingContainer.style.display = 'block';
+        predictButton.disabled = true;
+        const file = imageUploadButton.files[0];
+
+        const upload_body = new FormData();
+        upload_body.append('file', file);
+        const upload_request = await fetch(
+            '/api/upload_file',
+            {
+                method: 'POST',
+                body: upload_body
+            }
+        )
+        if (!upload_request.ok) {
+            console.error('Failed to upload file');
+            return;
+        }
+        const upload_response = await upload_request.json();
+        const file_hash = upload_response.file_hash;
+
+        const prediction_request = await fetch(
+            `/api/predict_age/${file_hash}`
+        );
+        if (!prediction_request.ok) {
+            console.error('Failed to predict age');
+            return;
+        }
+        const prediction_response = await prediction_request.json();
+        const age_index = prediction_response.predicted_class;
+        const age_range = age_ranges_index[age_index];
+        imageUploadContainer.style.display = 'none';
+        imagePreview.src = URL.createObjectURL(file);
+        imagePreview.style.display = 'block';
+        loadingContainer.style.display = 'none';
+        predictedAgeLabel.textContent = `Predicted Age: ${age_range}`;
+        predictionResultsContainer.style.display = 'block';
+        predictButton.disabled = false;
+    });
+
+    returnButton.addEventListener('click', function() {
+        imageUploadContainer.style.display = 'block';
+        predictButton.disabled = true;
+        imagePreview.style.display = 'none';
+        predictionResultsContainer.style.display = 'none';
+        imageUploadButton.value = '';
+    });
+});
